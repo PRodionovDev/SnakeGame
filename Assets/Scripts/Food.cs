7f170d874a388391b9fa7b3ec5d4dfe135916162
Pavel Rodionov <pavel.rodionov.developer@gmail.com>
2023-05-20 15:19:10 +0300

    Add sound

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    public AudioClip AudioClip;
+    public AudioSource AudioSource;
+    public GameObject snake;
+
+    void Start()
+    {
+        snake = GameObject.FindGameObjectWithTag("SnakeHead");
+        AudioSource = snake.GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("SnakeHead")) {
             other.GetComponent<SnakeMovement>().AddTail();
+            AudioSource.PlayOneShot(AudioClip);
             Destroy(gameObject);
         }
     }
